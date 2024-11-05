573c04ddf554ba5f7b2f938f2ab8d03815523d12
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-11-05 11:22:05 +0000

    fixed error with client sending messages to server. now works just fine

--- client.rb
+++ client.rb
@@ -1,38 +1,33 @@
-url = 'https://nathank.ngrok.pizza'
-headers = [
-  ["Authorization", "Bearer #{ENV['OPENAI_API_KEY']}"],
-  ["OpenAI-Beta", "realtime=v1"]
-]
-
+#!/usr/bin/env ruby
 require 'async'
 require 'async/http/endpoint'
 require 'async/websocket/client'
 
-USER = ARGV.pop || "anonymous"
 #URL = url  + '/twilio-stream'
+#URL = "http://localhost:9292/ai-stream"
 URL = "http://localhost:9292/ai-stream"
 
 Async do |task|
-  # Work around for NGrok fake http2
-  endpoint = Async::HTTP::Endpoint.parse(URL)
-  #endpoint = Async::HTTP::Endpoint.parse(URL, alpn_protocols: Async::HTTP::Protocol::HTTP11.names)
+  # apln_protocols is a workaround for NGrok fake http2
+  endpoint = Async::HTTP::Endpoint.parse(URL, alpn_protocols: Async::HTTP::Protocol::HTTP11.names)
 	
 	Async::WebSocket::Client.connect(endpoint) do |connection|
 		input_task = task.async do
 			while line = $stdin.gets
-				connection.write({user: USER, text: line})
+		    connection.write(Protocol::WebSocket::TextMessage.generate({
+          text: line.chomp
+        }))
 				connection.flush
 			end
 		end
 		
 		# Generate a text message by geneating a JSON payload from a hash:
 		connection.write(Protocol::WebSocket::TextMessage.generate({
-			user: USER,
-			status: "connected",
+			status: "connected"
 		}))
 		
 		while message = connection.read
-			puts message.inspect
+			puts "[response] #{message.inspect}"
 		end
 	ensure
 		input_task&.stop
